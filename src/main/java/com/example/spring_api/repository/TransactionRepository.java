7a46b5b8e80c06728b243b51b01adfd344d532fc
Alfonso <alfonsovillanueva.00@gmail.com>
2025-01-15 13:38:16 +0000

    Creating repository

---
+++ src/main/java/com/example/spring_api/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_api.repository;
+
+import com.example.spring_api.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
+
+
