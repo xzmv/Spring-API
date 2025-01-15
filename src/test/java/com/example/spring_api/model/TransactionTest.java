357bdfc4cc2d77826b86f94c9b20b9229c01c02c
Alfonso <alfonsovillanueva.00@gmail.com>
2025-01-15 13:34:19 +0000

    Adding tests to Transaction Model

---
+++ src/test/java/com/example/spring_api/model/TransactionTest.java
@@ -0,0 +1,54 @@
+package com.example.spring_api.model;
+
+import org.junit.jupiter.api.Test;
+
+class TransactionTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void getTransactionDate() {
+    }
+
+    @Test
+    void setTransactionDate() {
+    }
+
+    @Test
+    void getScheduleDate() {
+    }
+
+    @Test
+    void setScheduleDate() {
+    }
+
+    @Test
+    void getTransferFee() {
+    }
+
+    @Test
+    void setTransferFee() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+}
