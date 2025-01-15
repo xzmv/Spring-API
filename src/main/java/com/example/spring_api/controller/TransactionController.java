29bbb864fb43c4872c3200ddc85da9d2f3176d3b
Alfonso <alfonsovillanueva.00@gmail.com>
2025-01-15 15:44:39 +0000

    Creating routes for CRUD operations

---
+++ src/main/java/com/example/spring_api/controller/TransactionController.java
@@ -0,0 +1,46 @@
+package com.example.spring_api.controller;
+
+import com.example.spring_api.model.Transaction;
+import com.example.spring_api.services.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    private final TransactionService service;
+
+    public TransactionController(TransactionService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
+        return new ResponseEntity<>(service.createTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getTransactionById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        return ResponseEntity.ok(service.getAllTransactions());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> updateTransaction(@PathVariable Long id, @RequestBody Transaction transaction) {
+        return ResponseEntity.ok(service.updateTransaction(id, transaction));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
+        service.deleteTransaction(id);
+        return ResponseEntity.noContent().build();
+    }
+}
