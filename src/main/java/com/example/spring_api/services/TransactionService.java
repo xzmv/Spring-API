5cd78556523bf14005f4a5b5bd75862e21ac5981
Alfonso <alfonsovillanueva.00@gmail.com>
2025-01-16 11:22:20 +0000

    Adding calculateFee to Transaction Service

--- src/main/java/com/example/spring_api/services/TransactionService.java
+++ src/main/java/com/example/spring_api/services/TransactionService.java
@@ -3,6 +3,9 @@ package com.example.spring_api.services;
 import com.example.spring_api.model.Transaction;
 import com.example.spring_api.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -15,6 +18,7 @@ public class TransactionService {
     }
 
     public Transaction createTransaction(Transaction transaction) {
+        transaction.setTransferFee(calculateFee(transaction));
         return repository.save(transaction);
     }
 
@@ -34,11 +38,37 @@ public class TransactionService {
         existingTransaction.setDescription(updatedTransaction.getDescription());
         existingTransaction.setTransactionDate(updatedTransaction.getTransactionDate());
         existingTransaction.setScheduleDate(updatedTransaction.getScheduleDate());
+        existingTransaction.setTransferFee(calculateFee(updatedTransaction));
 
         return repository.save(existingTransaction);
     }
 
     public void deleteTransaction(Long id) {
         repository.deleteById(id);
+    }
+
+    private Double calculateFee(Transaction transaction) {
+        Double amount = transaction.getAmount();
+        LocalDate transactionDate = transaction.getTransactionDate();
+        LocalDate scheduleDate = transaction.getScheduleDate();
+
+        long daysBetween = ChronoUnit.DAYS.between(transactionDate, scheduleDate);
+
+        if (amount <= 1000.0 && daysBetween == 0) {
+            return (amount * 0.03) + 3.0;
+        } else if (amount <= 2000.0 && daysBetween >= 1 && daysBetween <= 10) {
+            return amount * 0.09;
+        } else if (amount > 2000.0) {
+            if (daysBetween >= 11 && daysBetween <= 20) {
+                return amount * 0.082;
+            } else if (daysBetween >= 21 && daysBetween <= 30) {
+                return amount * 0.069;
+            } else if (daysBetween >= 31 && daysBetween <= 40) {
+                return amount * 0.047;
+            } else if (daysBetween > 40) {
+                return amount * 0.017;
+            }
+        }
+        throw new IllegalArgumentException("Invalid scheduling date or amount");
     }
 }
